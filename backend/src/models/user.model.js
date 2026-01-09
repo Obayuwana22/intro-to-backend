6c4071f231f00dbe10c6674cd6b33eec2935c17f
Success Obayuwana <succcessful.obayuwana@charisol.io>
2026-01-09 09:20:55 +0100

    feat: add other auth APIs(login and logout)

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -30,4 +31,16 @@ const userSchema = new Schema(
   }
 );
 
-export const User = mongoose.model("User", userSchema)
+// before saving any password we need to hash it
+userSchema.pre("save", async function (next) {
+  if (!this.isModified(password)) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// compare passwords
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
